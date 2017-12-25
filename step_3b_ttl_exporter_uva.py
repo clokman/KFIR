cc8f479ce702f77eca60335874123996726d6b27
Clokman <john.c.lokman@gmail.com>
2017-12-25 13:53:19 +0100

    Separated steps for each bibliography and added 'hasOriginBibliography' property.

--- step_3b_ttl_exporter_uva.py
+++ step_3b_ttl_exporter_uva.py
@@ -4,7 +4,7 @@ from step_2b_triple_creator_uva import triples_list
 
 # IMPORTANT: CHANGE FILE NAME WITH EACH NEW VERSION IF THE FILE IS TO BE IMPORTED TO PROTEGE.
 # Protege does not always understand that this is a new file if the file name is the same with a previously imported file.
-output_file_path = 'Output//uva_1.10.ttl'
+output_file_path = 'Output//uva_1.11.ttl'
 file_obj = open(output_file_path, "w")
 # file_obj = open("Output//pure_bib_limited_0.6.5.ttl", "w")  # use for test version
 
