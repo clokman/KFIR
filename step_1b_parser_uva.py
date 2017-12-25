f2fb886134e64b214ca5b3d21532b82ceaf7fd2f
Clokman <john.c.lokman@gmail.com>
2017-12-25 13:51:58 +0100

    Separated steps for each bibliography and added 'hasOriginBibliography' property.

--- step_1b_parser_uva.py
+++ step_1b_parser_uva.py
@@ -2,4 +2,4 @@ from triplicator.bibliographyInstantiator import Bibliography
 
 # UvA bibliography
 uva_bibliography = Bibliography()
-uva_bibliography.importBib('Input//UvA_Pure_research_output-41217-edit.bib', verbose_import=True)
+uva_bibliography.importBib('Input//UvA_Pure_research_output-41217-edit.bib', verbose_import=False)
