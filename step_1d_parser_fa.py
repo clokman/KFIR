dc423e4f4820e9eef51175d505a03651e316a9a8
Clokman <john.c.lokman@gmail.com>
2018-01-05 13:23:21 +0100

    Added fa (Finn-Aron) files.

---
+++ step_1d_parser_fa.py
@@ -0,0 +1,6 @@
+from triplicator.bibliographyInstantiator import Bibliography
+
+# Finn and Aron bibliography
+fa_bibliography = Bibliography()
+fa_bibliography.importBib('Input//finn_aron//publications_finn_aron_edit.bib', verbose_import=True)
+
---
+++ step_3d_ttl_exporter_fa.py
@@ -0,0 +1,20 @@
+#### FINN & ARON ####################################################################################################
+
+from step_2d_triple_creator_fa import triples_list
+
+# IMPORTANT: CHANGE FILE NAME WITH EACH NEW VERSION IF THE FILE IS TO BE IMPORTED TO PROTEGE.
+# Protege does not always understand that this is a new file if the file name is the same with a previously imported file.
+output_file_path = "Output//finn_aron_1.0-DEMO.ttl"
+#output_file_path = "Output//vu_full-0.10.ttl"
+# output_file_path = "Output//problematic_characters_test-0.1.ttl"
+file_obj = open(output_file_path, "w")
+
+for each_triple in triples_list:
+    # TODO: this try-except block is a workaround and should be enhanced
+    try:
+        file_obj.write(each_triple)
+        file_obj.write('\n')
+    except:
+        pass
+
+print ('Success: The triples are written to the file "' + output_file_path + '".' )
