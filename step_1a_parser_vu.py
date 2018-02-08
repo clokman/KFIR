c354dbfaf5ac703f9121c2ae87512cac5bb8566a
Clokman <john.c.lokman@gmail.com>
2018-02-08 07:35:35 +0100

    bibtex cleaning library added to preprocessor, and enrichment functionality added to triplicator

--- step_1a_parser_vu.py
+++ step_1a_parser_vu.py
@@ -2,7 +2,7 @@ from triplicator.bibliographyInstantiator import Bibliography
 from preprocessor.Text_File import Text_File
 
 
-vu_bib_file = Text_File('Input//VU_Pure_research_output-51017.bib')
+vu_bib_file = Text_File('Input//VU_Pure_research_output-51017-edit.bib')
 vu_bib_file.clean_bibtex_file_and_output_cleaned_file(patterns_to_replace={
     '<': '--',
     '>': '--',
@@ -11,7 +11,7 @@ vu_bib_file.clean_bibtex_file_and_output_cleaned_file(patterns_to_replace={
     '/': '--',
     })
 
-uva_bib_file = Text_File('Input//UvA_Pure_research_output-41217.bib')
+uva_bib_file = Text_File('Input//UvA_Pure_research_output-41217-edit.bib')
 uva_bib_file.clean_bibtex_file_and_output_cleaned_file(patterns_to_replace={
     '<': '--',
     '>': '--',
@@ -21,10 +21,10 @@ uva_bib_file.clean_bibtex_file_and_output_cleaned_file(patterns_to_replace={
     })
 
 vu_bibliography = Bibliography()
-vu_bibliography.importBib('Input//VU_Pure_research_output-51017_cleaned.bib', verbose_import=False)
+vu_bibliography.importBib('Input//VU_Pure_research_output-51017-edit_cleaned.bib', verbose_import=False)
 
 uva_bibliography = Bibliography()
-uva_bibliography.importBib('Input//UvA_Pure_research_output-41217_cleaned.bib', verbose_import=False)
+uva_bibliography.importBib('Input//UvA_Pure_research_output-41217-edit_cleaned.bib', verbose_import=False)
 
 vu_bibliography.enrich(uva_bibliography, field_to_match_in_bibliographies='b_doi', method='merge')
 
