114c0300a700580556303cf3b9f69882b80363ec
Clokman <john.c.lokman@gmail.com>
2018-02-14 21:29:10 +0100

    Post operation console message added to point to the log file created

--- step_3a_ttl_exporter_vu.py
+++ step_3a_ttl_exporter_vu.py
@@ -1,6 +1,7 @@
 #### VU ################################################################################################################
 
 from step_2a_triple_creator_vu import triples_list
+from step_1a_parser_vu import log_file
 
 # IMPORTANT: CHANGE FILE NAME WITH EACH NEW VERSION IF THE FILE IS TO BE IMPORTED TO PROTEGE.
 # Protege does not always understand that this is a new file if the file name is the same with a previously imported file.
@@ -17,7 +18,8 @@ for each_triple in triples_list:
         pass
 
 
-print('Success: The triples are written to the file "' + output_file_path + '".' )
+print('Success: The triples are written to "%s."' % output_file_path)
+print('A log of the operation is kept in "%s".' % log_file.input_file_path)
 
 # NOTE: Check the integrity of the produced .ttl file in command line
 # > ttl <path to file>
