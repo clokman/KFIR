8c63c0474233124f3ede392d1f668b3630b4b5fc
Clokman <john.c.lokman@gmail.com>
2018-02-27 18:08:12 +0100

    Cleaned main directory from old scripts

--- step_1d_parser_fa.py
+++
@@ -1,6 +0,0 @@
-from triplicator.bibTools import Bibliography
-
-# Finn and Aron bibliography
-fa_bibliography = Bibliography()
-fa_bibliography.importBib('Input//finn_aron//publications_finn_aron_edit.bib', verbose_import=True)
-
