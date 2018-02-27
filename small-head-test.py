ae83bb5f4944ddc64fefdee00358047e5e4f32f3
Clokman <john.c.lokman@gmail.com>
2018-02-27 18:08:01 +0100

    Cleaned main directory from old scripts

---
+++ small-head-test.py
@@ -0,0 +1,4 @@
+from triplicator.bibTools import Bibtex_File
+
+my_bibtex_file = Bibtex_File('Input//vu_100k_feb.bib')
+my_bibtex_file.convert_to_ttl(desired_version='2.1-objtest2', desired_source_bibliography_name='vu_100k_feb')
