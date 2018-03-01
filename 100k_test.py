6060d46bb6a289a9bd4d39ae8a2b1b45b92c4d23
Clokman <john.c.lokman@gmail.com>
2018-03-01 14:02:07 +0100

    Renamed file

--- 100k_test.py
+++
@@ -1,4 +0,0 @@
-from triplicator.bibTools import Bibtex_File
-
-my_bibtex_file = Bibtex_File('Input//vu_100k_feb.bib')
-my_bibtex_file.convert_to_ttl(desired_version='2.1-objtest2', desired_source_bibliography_name='vu_100k_feb')
