c4b6f443fc3ba5939e1f02d2da1dbc1858a774b5
Clokman <john.c.lokman@gmail.com>
2018-03-01 14:05:57 +0100

    ttl version change

--- head_test.py
+++ head_test.py
@@ -1,4 +1,4 @@
 from triplicator.bibTools import Bibtex_File
 
 my_bibtex_file = Bibtex_File('Input//vu_100k_feb.bib')
-my_bibtex_file.convert_to_ttl(desired_version='2.1-objtest2', desired_source_bibliography_name='vu-100k-feb')
+my_bibtex_file.convert_to_ttl(desired_version='2.1_post_refactor', desired_source_bibliography_name='vu-100k-feb')
