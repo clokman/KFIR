f6f23c4448b7bd1ec8d42ff2354138c39a7b8b06
Clokman <john.c.lokman@gmail.com>
2018-03-01 14:27:01 +0100

    Changed ttl versions

--- finn.py
+++ finn.py
@@ -3,4 +3,4 @@
 from triplicator.bibTools import Bibtex_File
 
 my_bibtex_file = Bibtex_File('Input//finn_aron//publications_finn_aron.bib')
-my_bibtex_file.convert_to_ttl(desired_version='2.1_post_refactor', desired_source_bibliography_name='VU-Finn')
+my_bibtex_file.convert_to_ttl(desired_version='v2.1_post_refactor', desired_source_bibliography_name='VU-Finn')
--- vu_pure.py
+++ vu_pure.py
@@ -3,5 +3,5 @@
 from triplicator.bibTools import Bibtex_File
 
 my_bibtex_file = Bibtex_File('Input//VU_Pure_research_output-51017.bib')
-my_bibtex_file.convert_to_ttl(desired_version='2.1_post_refactor', desired_source_bibliography_name='VU-Pure',
+my_bibtex_file.convert_to_ttl(desired_version='v2.1_post_refactor', desired_source_bibliography_name='VU-Pure',
                               output_directory='Output')
