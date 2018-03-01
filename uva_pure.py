69eddeb20730793b3717f57367b3891c447c0af7
Clokman <john.c.lokman@gmail.com>
2018-03-01 14:23:54 +0100

    Changed ttl output directory to 'Output'

--- uva_pure.py
+++ uva_pure.py
@@ -3,4 +3,5 @@
 from triplicator.bibTools import Bibtex_File
 
 my_bibtex_file = Bibtex_File('Input//UvA_Pure_research_output-41217.bib')
-my_bibtex_file.convert_to_ttl(desired_version='2.1_post_refactor', desired_source_bibliography_name='UvA-Pure')
+my_bibtex_file.convert_to_ttl(desired_version='2.1_post_refactor', desired_source_bibliography_name='UvA-Pure',
+                              output_directory='Output')
