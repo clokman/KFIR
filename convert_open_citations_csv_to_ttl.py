0fbb8ffa11fb21c9d991cbbc61064cfaf3719a46
Clokman <john.c.lokman@gmail.com>
2018-03-22 18:29:50 +0100

    Updated the version of OpenCitations ttl output to 1.3.3

--- convert_open_citations_csv_to_ttl.py
+++ convert_open_citations_csv_to_ttl.py
@@ -1,19 +1,5 @@
 from triplicator.bibTools import Bibliography
 
-
-# clean, merge multi-row entries, and write to file
-#oc_csv = CSV_File(input_file_path, column_delimiter_pattern_in_input_file =' , ')
-
-#oc_csv.set_parsing_and_cleaning_parameters(line_head_pattern_to_remove='', line_tail_pattern_to_remove=' ,',
-#                                           cell_head_and_tail_characters_to_remove='"')
-
-#oc_csv.set_output_formatting_parameters(column_separator='same as input', cell_value_separator=' | ',
-#                                        line_head=' ', line_tail=' ,',
-#                                        cell_wrapper='"')
-
-#oc_csv.live_clean_and_row_merge(0)
-
-
 ######## Parse the csv file as a Bibliography object #########
 oc_bibliography = Bibliography()
 oc_bibliography.importCsv(path_of_file_to_import='Input//oc_articles_with_matching_dois_v1.3.csv',
@@ -33,5 +19,5 @@ oc_triples.import_bibliography_object(oc_bibliography, desired_source_bibliograp
 
 ################### Write to .ttl file ######################
 from triplicator.rdfTools import RDF_File
-oc_ttl_file = RDF_File('Output//oc_articles_with_matching_dois_v1.3.2.ttl')
+oc_ttl_file = RDF_File('Output//oc_articles_with_matching_dois_v1.3.3.ttl')
 oc_ttl_file.write_triples_to_file(oc_triples)
