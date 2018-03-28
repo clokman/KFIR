afee970764860b1eef3e896c71996b1675e9442e
Clokman <john.c.lokman@gmail.com>
2018-03-28 13:55:27 +0200

    Output ttl version for acceptance tests updated to 0.7

--- acceptance_tests/merge_vu_uva_opencitations_and_convert_to_ttl.py
+++ acceptance_tests/merge_vu_uva_opencitations_and_convert_to_ttl.py
@@ -48,6 +48,6 @@ merged_triples.import_bibliography_object(enriched_bibliography,
                                           show_progress_bar=True)
 
 ### Write to ttl file ###
-ttl_file = RDF_File('acceptance_tests//test_data//output//vu_and_uva_merged_and_enriched_with_opencitations_v0.6_10k.ttl')  # PATH
+ttl_file = RDF_File('acceptance_tests//test_data//output//vu_and_uva_merged_and_enriched_with_opencitations_v0.7_10k.ttl')  # PATH
 ttl_file.write_triples_to_file(merged_triples, show_progress_bar=True)
 
