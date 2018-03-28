190c233c6760469903a000a45d63fec964bcba98
Clokman <john.c.lokman@gmail.com>
2018-03-28 22:29:48 +0200

    Updated merge/enrichment/ttl-conversion script and changed version to 0.2 (code adapted from acceptance tests).

--- _04_merge_vu_uva_opencitations_and_convert_to_ttl.py
+++ _04_merge_vu_uva_opencitations_and_convert_to_ttl.py
@@ -1,14 +1,19 @@
 from triplicator.bibTools import Bibliography
 from triplicator.rdfTools import Triples, RDF_File
+from meta.consoleOutput import ConsoleOutput
+
+console = ConsoleOutput('log.txt')
+console.clear_log_file()
 
 vu_bibliography = Bibliography()
-vu_bibliography.importBibtex('Input//VU_Pure_research_output-51017_cleaned.bib', show_progress_bar=True)
+vu_bibliography.importBibtex('Input//VU_Pure_research_output-51017_cleaned.bib', show_progress_bar=True)  # PATH
 
 uva_bibliography = Bibliography()
-uva_bibliography.importBibtex('Input//UvA_Pure_research_output-41217_cleaned.bib', show_progress_bar=True)
+uva_bibliography.importBibtex('Input//UvA_Pure_research_output-41217_cleaned.bib', show_progress_bar=True)  # PATH
+
 
 oc_bibliography = Bibliography()
-oc_bibliography.importCsv(path_of_file_to_import='Input//oc_articles_with_matching_dois_v1.3.csv',
+oc_bibliography.importCsv(path_of_file_to_import='Input//oc_articles_with_matching_dois_v1.3.csv',  # PATH
                           csv_delimiter_character=',',
                           field_value_list_separator=' | ',
                           id_column_header='doi',
@@ -16,23 +21,32 @@ oc_bibliography.importCsv(path_of_file_to_import='Input//oc_articles_with_matchi
                           cleaning_algorithm='default',
                           show_progress_bar=True)
 
-vu_bibliography.enrich_with(uva_bibliography,
+
+enriched_bibliography = Bibliography()
+# Initiates as the same with VU... The previously created vu_bibliography variable is kept untouched in order to make
+# post-operation comparisons possible, in case they would be needed
+enriched_bibliography.importBibtex('Input//VU_Pure_research_output-51017_cleaned.bib', show_progress_bar=True)  # PATH
+console.log_message('enriched_bibliography initiated with VU Bibliography object')
+
+enriched_bibliography.enrich_with(uva_bibliography,
                             field_to_match_in_bibliographies='b_doi',
                             method='merge')
+console.log_message('enriched_bibliography enriched with UvA Bibliography object')
 
-vu_bibliography.enrich_with(oc_bibliography,
+enriched_bibliography.enrich_with(oc_bibliography,
                             field_to_match_in_bibliographies='b_doi',
                             method='left join')
+console.log_message('enriched_bibliography enriched with OpenCitations Bibliography object')
 
 # TODO: Turn n3 transformation and ttl writing steps to a single 'write_to_ttl' method for Bibliography (e.g., vu_bibliography.write_to_ttl())
 # TODO: Source bibliography name for individual bibliographies inside must be preserved by allowing specification during parsing to Bibliography object and not during ttl conversion. (currently only one can be specified during ttl conversion)
 ### Convert to n3 format ###
 merged_triples = Triples()
-merged_triples.import_bibliography_object(vu_bibliography,
+merged_triples.import_bibliography_object(enriched_bibliography,
                                           desired_source_bibliography_name='vu_and_uva_enriched_with_opencitations',
                                           show_progress_bar=True)
 
 ### Write to ttl file ###
-ttl_file = RDF_File('Output//vu_and_uva_merged_and_enriched_with_opencitations_v01.ttl')
+ttl_file = RDF_File('Output//vu_and_uva_merged_and_enriched_with_opencitations_v0.2.ttl')  # PATH
 ttl_file.write_triples_to_file(merged_triples, show_progress_bar=True)
 
