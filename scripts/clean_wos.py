bad1387ae518e51c35542f53057fbcc0cf9eff1d
Clokman <john.c.lokman@gmail.com>
2018-04-18 15:07:20 +0200

    Updated WOS cleaning script to show a progress bar and progress completion message.

--- scripts/clean_wos.py
+++ scripts/clean_wos.py
@@ -4,15 +4,22 @@ if os.getcwd()[-7:] == 'scripts':
 
 from preprocessor.Text_File import Text_File
 from preprocessor.string_tools import String
+from meta.consoleOutput import ConsoleOutput
 
+console = ConsoleOutput(log_file_path='log.txt')
+wos_file_used_for_logging = Text_File('Input//web_of_science_uva_vu//data_uvavu000001.ttl')  # PATH
+maximum_progress = wos_file_used_for_logging.get_no_of_lines_in_file()
 
-with open('Input//web_of_science_uva_vu//data_uvavu000001.ttl', encoding='utf8') as original_wos_file:
-    with open('Input//web_of_science_uva_vu//data_uvavu000001_cleaned.ttl', mode='a', encoding='utf8') as cleaned_wos_file:
+with open('Input//web_of_science_uva_vu//data_uvavu000001.ttl', encoding='utf8') as original_wos_file:  # PATH
+    with open('Input//web_of_science_uva_vu//data_uvavu000001_cleaned.ttl', mode='a', encoding='utf8') as cleaned_wos_file:  # PATH
 
-        for each_line in original_wos_file:
+        for i, each_line in enumerate(original_wos_file):
             each_line = String(each_line)
             each_line.purify(clean_from_non_ascii_characters=True,
                              clean_newline_characters=True,
                              remove_problematic_patterns=False)
 
             cleaned_wos_file.writelines(each_line)
+            console.print_current_progress(i, maximum_progress, "Cleaning file '%s'" % wos_file_used_for_logging.input_file_path)
+
+console.log_message("Cleaning of the file '%s' finished" % wos_file_used_for_logging.input_file_path)
