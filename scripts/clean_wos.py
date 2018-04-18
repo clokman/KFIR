26ad803fe9e7e0540ea5080cda62009e42b47633
Clokman <john.c.lokman@gmail.com>
2018-04-18 13:45:28 +0200

    Added scripts directory and created the Web of Science cleaning script in it. All scripts in the root directory will be moved to this directory when convenient.

---
+++ scripts/clean_wos.py
@@ -0,0 +1,14 @@
+from preprocessor.Text_File import Text_File
+from preprocessor.string_tools import String
+
+
+with open('Input//web_of_science_uva_vu//data_uvavu000001.ttl', encoding='utf8') as original_wos_file:
+    with open('Input//web_of_science_uva_vu//data_uvavu000001_cleaned.ttl', mode='a', encoding='utf8') as cleaned_wos_file:
+
+        for each_line in original_wos_file:
+            each_line = String(each_line)
+            each_line.purify(clean_from_non_ascii_characters=True,
+                             clean_newline_characters=True,
+                             remove_problematic_patterns=False)
+
+            cleaned_wos_file.writelines(each_line)
