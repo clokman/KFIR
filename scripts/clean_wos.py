3106e47c95d7dc6d47b9cb902412c756864330e6
Clokman <john.c.lokman@gmail.com>
2018-04-18 14:06:50 +0200

    Updated WOS cleaning script to run from the scripts subfolder

--- scripts/clean_wos.py
+++ scripts/clean_wos.py
@@ -1,9 +1,13 @@
+import os
+if os.getcwd()[-7:] == 'scripts':
+    os.chdir('..//')
+
 from preprocessor.Text_File import Text_File
 from preprocessor.string_tools import String
 
 
-with open('Input//web_of_science_uva_vu//data_uvavu000001.ttl', encoding='utf8') as original_wos_file:
-    with open('Input//web_of_science_uva_vu//data_uvavu000001_cleaned.ttl', mode='a', encoding='utf8') as cleaned_wos_file:
+with open('..//Input//web_of_science_uva_vu//data_uvavu000001.ttl', encoding='utf8') as original_wos_file:
+    with open('..//Input//web_of_science_uva_vu//data_uvavu000001_cleaned.ttl', mode='a', encoding='utf8') as cleaned_wos_file:
 
         for each_line in original_wos_file:
             each_line = String(each_line)
